8bc2967656aca42adcb1539eee249a7da9ec1ffd
Vinayak2208 <krvinayak2208@gmail.com>
2024-12-03 23:54:27 +0530

    first commit

---
+++ Registration.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+    <link rel="stylesheet" href="style.css">
+
+    <title>Farmers infomation</title>
+</head>
+<body>
+
+    <nav class="nav">
+
+      <input type="checkbox" id="check">
+      <label for="check" class="checkbtn">
+        <i class="fas fa-bars"></i>
+      </label>
+
+
+        <div class="container">
+            <h1 class="logo"> <a href="/index.html">Farmers help</a></h1>
+
+            <ul>
+                <li><a href="#" class="active">Home</a></li>
+                <li><a href="#crop_headers">Crops</a></li>
+                <li><a href="#vegetable_headers">Vegetable</a></li>
+                <li><a href="#fruit_headers">Fruits</a></li>
+                <li><a href="technology.html">Technologies</a></li>
+            </ul>
+
+
+        </div>
+    </nav>
+
